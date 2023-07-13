1d0bc4e54409e437327e15665e499388e4686cd0
Kaity-l <kate.logan13@gmail.com>
2023-07-13 13:43:56 -0700

    5-printf.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - The main function should print the stated text.
+ *
+ *Return: 0 when successful.
+ */
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return(0);
+}
